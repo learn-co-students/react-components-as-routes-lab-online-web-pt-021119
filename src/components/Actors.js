0ffb76d98554bd62b36aa556e9249cc9b61c12a9
Tracy Ngo <tngo1206@gmail.com>
2019-11-20 17:57:08 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,13 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor =>
+        <div className='actor'>
+        <p>Name: {actor.name}</p>
+        <p>Movies: <ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul></p>
+        </div>)
+      }
     </div>
   );
 };
