ac958066540a60a0d99c417961751d4fa21726a4
Eyal Toledano <eutait@gmail.com>
2019-12-02 13:22:42 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map(movie =>
+          <div>
+            {movie.title} at {movie.time}
+            <ul>
+            { movie.genres.map(genre =>
+              <li>{genre}</li>
+            ) }
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
